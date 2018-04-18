578a396909890cba52661c81346dcde5cc6a1c03
Pierre Abillama <abill001@umn.edu>
2018-04-18 15:20:53 -0500

    Sensor setup added

--- sensor.c
+++ sensor.c
@@ -1,16 +1,23 @@
 #include "xc.h"
 #include "sensor.h"
 
-void sensor_calibrate()
-{
-    //sorry, testing git
-}
+//Calibrate Sensor using Arduino
+
+/*SAMC = 31
+ *ADCS = 64
+ *SSRC = 0b111 (auto-convert)
+ *ASAM = 1
+ *AN0 is used
+ */
 
 void sensor_init()
 {
-    AD1CON1=0;
+    AD1CON1bits.SSRC=0b111;
+    AD1CON1bits.ASAM = 1;
+    AD1CON3bits.SAMC = 31;
+    AD1CON3bits.ADCS = 63;
     AD1CON2=0;
-    AD1CON3=0;
-    
+    AD1PCFGbits.PCFG0 = 0;
+    _TRISA0 = 1;
 }
 
