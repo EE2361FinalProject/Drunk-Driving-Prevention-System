214d9d4e0de35f072c8671d0a062f41e07341ab7
Pierre Abillama <abill001@umn.edu>
2018-04-21 11:26:21 -0500

    Added iLed config function

--- ILEDFinal.c
+++ ILEDFinal.c
@@ -3,22 +3,24 @@
 #include "levac_lab2b_asm_004.h"
 #include "ILEDFinal.h"
 
-volatile int i= 0;
+volatile int i = 0;
 
-void turnonwheel (void) {
-     TRISBbits.TRISB13 = 0;
-     LATBbits.LATB13 = 0;
-     _T5IE=1;
-     _T5IF=0;
-     TMR5=0;
-     PR5= 1000;
-     T5CON = 0x8030;
+void iLED_setup(void) {
+    TRISBbits.TRISB13 = 0;
+    LATBbits.LATB13 = 0;
 }
 
-void turnoffwheel (void) {
+void turnonwheel(void) {
+    _T5IF = 0;
+    _T5IE = 1;
+    TMR5 = 0;
+    PR5 = 1000;
+    T5CON = 0x8030;
+}
+
+void turnoffwheel(void) {
     T5CONbits.TON = 0;
 }
-
 
 uint32_t packColor(unsigned char Red, unsigned char Blu, unsigned char Grn) {
     unsigned long int RGBval = 0;
@@ -51,7 +53,6 @@ void writePacCol(uint32_t PackedColor) {
     writeColor(getR(PackedColor), getG(PackedColor), getB(PackedColor));
 }
 
-
 uint32_t Wheel(unsigned char WheelPos) {
     WheelPos = 255 - WheelPos;
 
@@ -66,55 +67,49 @@ uint32_t Wheel(unsigned char WheelPos) {
     return packColor(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-
-
 void writeColor(int r, int g, int b) {
-    int shiftr;
-    int shiftg;
-    int shiftb;
     int i = 7;
-    int p = 7;
+    int j = 7;
     int k = 7;
+    int redShift;
+    int blueShift;
+    int greenShift;
+    LATBbits.LATB13 = 0;
     
-    while (i >= 0) {
-        shiftr = 1 << i;
-        if ((r & shiftr) > 0) {
+    delay_100us();
+        while (i >= 0) {
+        redShift = 1 << i;
+        if ((redShift & r) > 0)
             write_1();
-        } else {
+        else
             write_0();
-        }
         i = i - 1;
     }
-
-    while (p >= 0) {
-        shiftg = 1 << p;
-        if ((g & shiftg) > 0) {
+    while (j >= 0) {
+        greenShift = 1 << j;
+        if ((greenShift & g) > 0)
             write_1();
-        } else {
+        else
             write_0();
-        }
-        p = p - 1;
+        j = j - 1;
     }
-
     while (k >= 0) {
-        shiftb = 1 << k;
-        if ((b & shiftb) > 0) {
+        blueShift = 1 << k;
+        if ((blueShift & b) > 0)
             write_1();
-        } else {
+        else
             write_0();
-        }
         k = k - 1;
     }
 
 }
 
-
-void __attribute__((__interrupt__,__auto_psv__)) _T5Interrupt(void){
+void __attribute__((__interrupt__, __auto_psv__)) _T5Interrupt(void) {
     _T5IF = 0;
-    
+
     writePacCol(Wheel(i));
-        i=(i+1)%255;
-        
+    i = (i + 1) % 255;
+
 }
 
 
