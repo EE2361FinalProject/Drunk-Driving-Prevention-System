594d03553e7e75ae8b99ff6a4bf44006fc493861
Pierre Abillama <abill001@umn.edu>
2018-04-29 17:57:06 -0500

    final touches

--- Particle_I2C.ino
+++ Particle_I2C.ino
@@ -1,9 +1,22 @@
 #include "application.h"
 
+
+#define ALCOHOL 0
+#define MOUTHWASH 1
+
+#define CONFIGURATION MOUTHWASH
+
+#if CONFIGURATION == MOUTHWASH
+#define THRESHOLD 62
+#define DIGITAL_TO_BAC_1 13
+#define DIGITAL_TO_BAC_2 10000.0
+#define DAC_OFFSET 0
+#elif CONFIGURATION == ALCOHOL
 #define THRESHOLD 115
 #define DIGITAL_TO_BAC_1 8.515
 #define DIGITAL_TO_BAC_2 10000.0
 #define DAC_OFFSET 21
+#endif
 
 bool publish = false;
 
@@ -28,7 +41,13 @@ void receiveEvent (int howMany) {
         }
         else {
            bac += Wire.read ();
-           realBAC = (bac- 21) * DIGITAL_TO_BAC_1 / DIGITAL_TO_BAC_2;
+           Serial.println(bac);
+           if (bac > DAC_OFFSET) {
+                realBAC = (bac - DAC_OFFSET) * DIGITAL_TO_BAC_1 / DIGITAL_TO_BAC_2;
+           }
+           else {
+               realBAC = 0.0;
+           }
         }
     }
 }
@@ -38,13 +57,18 @@ void receiveEvent (int howMany) {
  */
  
 void bacHandler (const char *event, const char *data) {
+   
     if (bac > THRESHOLD) {
         Particle.publish ("BAC_High");
     }
+    
 }
 
 
 void setup() {
+	
+	Serial.begin (9600);
+    
     Particle.connect();
     if (!Wire.isEnabled ()) {
         Wire.begin(0x1);
